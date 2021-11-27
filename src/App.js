6654abfe2d18b38fe42687104b82933f7b6982be
Tosin Akerele <akerele.james1699@gmail.com>
2021-11-27 11:24:50 +0100

    added some cool animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Header } from "./components/header/header";
 import StudentPage from "./components/studentspage/StudentPage";
+import Flip from "react-reveal/Flip";
 
 function App() {
   return (
     <>
-      <Header />
+      <Flip bottom>
+        <Header />
+      </Flip>
       <StudentPage />
     </>
   );
--- src/components/studentcards/StudentCard.jsx
+++ src/components/studentcards/StudentCard.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import "./StudentCard.css";
+import Fade from "react-reveal/Fade";
 // import oreoluwa from "../../students-images/OREOLUWA.jpg";
 // import styled from "styled-components";
 
 function StudentCard({ students }) {
   // console.log(students.image);
   return (
-    <section>
-      {students.map(({ firstName, lastName, image, id, emailadd, level }) => {
-        // console.log(image + "\n" + oreoluwa);
-        return (
-          <aside key={id} className={firstName}>
-            <div className={firstName + "-image"}>
-              <img src={image} alt={firstName + "-image"} />
-            </div>
-            {/* <ImageWrapper image={image} className={firstName + "-image"} /> */}
-            <div className="basic-info">
-              <h4>{firstName + " " + lastName} </h4>
-              <h5 className="level">Class: {level}</h5>
-              <div className="socials">
-                <a href={"mailto:" + emailadd}>
-                  <i class="far fa-envelope"></i>
-                </a>
-                <i class="fab fa-facebook-square"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
-                <i class="fab fa-whatsapp"></i>
-                <i class="fab fa-twitter"></i>
+    <Fade bottom big>
+      <section>
+        {students.map(({ firstName, lastName, image, id, emailadd, level }) => {
+          // console.log(image + "\n" + oreoluwa);
+          return (
+            <aside key={id} className={firstName}>
+              <div className={firstName + "-image"}>
+                <img src={image} alt={firstName + "-image"} />
               </div>
-            </div>
-          </aside>
-        );
-      })}
-    </section>
+              {/* <ImageWrapper image={image} className={firstName + "-image"} /> */}
+              <div className="basic-info">
+                <h4>{firstName + " " + lastName} </h4>
+                <h5 className="level">Class: {level}</h5>
+                <div className="socials">
+                  <a href={"mailto:" + emailadd}>
+                    <i class="far fa-envelope"></i>
+                  </a>
+                  <i class="fab fa-facebook-square"></i>
+                  <i class="fab fa-instagram"></i>
+                  <i class="fab fa-youtube"></i>
+                  <i class="fab fa-whatsapp"></i>
+                  <i class="fab fa-twitter"></i>
+                </div>
+              </div>
+            </aside>
+          );
+        })}
+      </section>
+    </Fade>
   );
 }
 
